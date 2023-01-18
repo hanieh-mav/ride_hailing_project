4ed1596677766cd911ac4aa7c1c8fedf86bb9bc9
hanieh <h.mehdiabadi@gmail.com>
2023-01-18 23:31:11 +0330

    add runtimeconfig

---
+++ src/service/config/runtime_config.py
@@ -0,0 +1,13 @@
+class RuntimeConfig:
+    DATABASE_DRIVER_NAME: str = ''
+    DATABASE_USERNAME: str = ''
+    DATABASE_PASSWORD: str = ''
+    DATABASE_HOST: str = ''
+    DATABASE_PORT: int = 0
+    DATABASE_NAME: str = ''
+    DATABASE_POOL_SIZE: int = 0
+    DATABASE_MAX_OVERFLOW: int = 0
+
+    CACHE_HOST: str = ''
+    CACHE_PORT: int = 0
+    CACHE_DB_NUMBER: int = 0
