bd36de3273dd9b6ed90875443cdd4b811009a4c9
hanieh <h.mehdiabadi@gmail.com>
2023-01-20 11:50:52 +0330

    write method for get count of request for region

--- src/service/core/service_app_service_contract/repository/iregion_request_repository.py
+++ src/service/core/service_app_service_contract/repository/iregion_request_repository.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 from src.service.core.service_model.region_request_model import RegionRequestModel
 
@@ -7,4 +8,8 @@ class IRegionRequestRepository(ABC):
 
     @abstractmethod
     def add(self, model: RegionRequestModel) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_count_of_request_for_region_id_in_datetime_range(self, region_id: int, datetime_value: datetime) -> int:
         raise NotImplementedError
--- src/service/infrastructure/repository/region_request_repository.py
+++ src/service/infrastructure/repository/region_request_repository.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from injector import inject
 
 from src.service.core.service_app_service_contract.connection.idatabase_connection import IDatabaseConnection
@@ -14,3 +16,8 @@ class RegionRequestRepository(IRegionRequestRepository):
     def add(self, model: RegionRequestModel) -> None:
         with self.__database_connection.ms_sql_server_session(has_transaction=True) as session:
             session.add(model)
+
+    def get_count_of_request_for_region_id_in_datetime_range(self, region_id: int, datetime_value: datetime) -> int:
+        with self.__database_connection.ms_sql_server_session() as session:
+            return session.query(RegionRequestModel).filter(RegionRequestModel.region_id == region_id).filter(
+                RegionRequestModel.entry_date >= datetime_value).count()
